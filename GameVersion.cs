e91a625a7861eef1c63fa70f19a418f37c284e42
ZondaKeN <marino.zirks@gmail.com>
2023-03-25 03:58:22 +0100

    Update GameVersion.cs

--- GameVersion.cs
+++ GameVersion.cs
@@ -18,7 +18,13 @@ public class GameVersion
     
     private static IDictionary<string, string> _versionMap = new Dictionary<string, string>()
     {
-        ["1.15.0.0"] = "1.15"
+        ["1.4.1.0"] = "1.04 reg 1.05",
+        ["1.8.0.0"] = "1.08 reg 1.22",
+        ["1.11.0.0"] = "1.11 reg 1.30",
+        ["1.12.0.0"] = "1.12 reg 1.32",
+        ["1.15.0.0"] = "1.15 reg 1.35",
+        ["1.15.1.0"] = "1.15 reg 1.35",
+        ["1.15.2.0"] = "1.15.2 reg 1.35",
     };
 
     public string Version
@@ -32,9 +38,10 @@ public class GameVersion
     }
 
     private static IOffsetData V1_15 => new OffsetData_1_15();
+    
     private static IDictionary<string, IOffsetData> _offsetMap => new Dictionary<string, IOffsetData>
     {
-        ["1.15"] = V1_15,
+        ["1.15 reg 1.35"] = V1_15,
     };
 
     public IOffsetData OffsetData => _offsetMap[Version];
